16acb228572350c803b4f3484c616035d6aadd96
Daru13 <daru14@gmail.com>
2017-09-28 19:01:37 +0200

    Removed the asynchronous part of the init script (it seems to be handled by require.js?).

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,13 +2,12 @@ import { RubEns } from "./RubEns";
 import { RubEnsParameters } from "./RubEnsParameters";
 
 // Global variable to hold the running instance of the app (for debug purposes)
-let rubens = null;
+document["rubens"] = null;
+console.log(document["rubens"]);
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Create a basic app (global for debug purposes)
-    rubens = new RubEns(new RubEnsParameters());
+// Create a basic app (global for debug purposes)
+document["rubens"] = new RubEns(new RubEnsParameters());
 
-    // Bind the single image to the canvas
-    let canvas = document.getElementById("main_drawing_canvas");
-    rubens.document.createImage(canvas);
-}, false);
+// Bind the single image to the canvas
+let canvas = document.getElementById("main_drawing_canvas");
+document["rubens"].document.createImage(canvas);
