3e35a4a640ec573a90b7864609dbeb2e70220bdd
Daru13 <daru14@gmail.com>
2017-09-28 02:00:59 +0200

    Added document and app-level parameters.

---
+++ src/ts/DocumentParameters.ts
@@ -0,0 +1,9 @@
+export class DocumentParameters {
+    // Title (required)
+    title: string = "New document";
+
+    // Dimensions (required)
+    width: number  = 800;
+    height: number = 600;
+
+}
---
+++ src/ts/RubEnsParameters.ts
@@ -0,0 +1,3 @@
+export class RubEnsParameters {
+    createDocumentOnStartup: boolean = true;
+}
