a3667709d8b51db5cc7e6bbc6637f06e7f5a7c36
Daru13 <daru14@gmail.com>
2017-10-01 02:18:46 +0200

    Added an EventManager instance to RUbEns class; including two small tests, to be removed.

--- src/ts/RubEns.ts
+++ src/ts/RubEns.ts
@@ -1,6 +1,7 @@
 import { RubEnsParameters } from "./RubEnsParameters";
 import { Document } from "./Document";
 import { DocumentParameters } from "./DocumentParameters";
+import { EventManager } from "./UI/EventManager";
 
 export class RubEns {
     static version: string = "0.1";
@@ -10,10 +11,29 @@ export class RubEns {
     // Currently modified document
     /*private*/ document: Document = null;
 
+    private eventManager: EventManager;
+
     constructor (parameters: RubEnsParameters) {
         if (parameters.createDocumentOnStartup) {
             this.createDocument(new DocumentParameters());
         }
+
+        // Start handling events in the UI
+        this.eventManager = new EventManager();
+        this.eventManager.startListening();
+
+        // Debug tests
+        this.eventManager.registerEventHandler({
+            eventTypes: ["click"],
+            selector: "canvas",
+            callback: (_) => console.log("Click event on canvas.")
+        });
+
+        this.eventManager.registerEventHandler({
+            eventTypes: ["mousemove"],
+            selector: "canvas",
+            callback: (_) => console.log("Move event on canvas.")
+        });
     }
 
     createDocument (parameters: DocumentParameters) {
