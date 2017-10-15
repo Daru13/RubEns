eb48cc3dd74f80e98f6ca5627e65c43079d8ba20
mfehr <mathieu.fehr@gmail.com>
2017-10-15 22:48:22 +0200

    Added documentation.

--- src/ts/SupportChecker.ts
+++ src/ts/SupportChecker.ts
@@ -1,9 +1,24 @@
+/**
+ * Class used to check support of some API for old browsers
+ */
 export class SupportChecker {
 
+    /**
+     * Check the support of everything the app needs
+     *
+     * @return return true if the browser support the necessary APIs
+     *
+     * @author Mathieu Fehr
+     */
     static checkSupport() : boolean {
         return SupportChecker.checkFileAPISupport();
     }
 
+    /**
+     * Check the support of the api used for loading files
+     *
+     * @returns return true if the browser support the necessary APIs
+     */
     static checkFileAPISupport() : boolean {
         return  "File" in window &&
                 "Blob" in window;
