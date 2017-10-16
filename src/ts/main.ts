51978045f932c7345c98a7b6ee4aaf32f7663d50
mfehr <mathieu.fehr@gmail.com>
2017-10-16 16:34:07 +0200

    Removed debug code in main.ts.

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,9 +1,5 @@
 import { RubEns } from "./RubEns";
 import { RubEnsParameters } from "./RubEnsParameters";
 
-// Global variable to hold the running instance of the app (for debug purposes)
-document["rubens"] = null;
-console.log(document["rubens"]);
-
 // Create a basic app (global for debug purposes)
 document["rubens"] = new RubEns(new RubEnsParameters());
