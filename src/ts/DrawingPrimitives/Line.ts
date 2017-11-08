aaaa9730c1f2f4d66dd2700946f27fcf54e4752e
mfehr <mathieu.fehr@gmail.com>
2017-11-08 05:03:45 +0100

    Refactored line drawing.

--- src/ts/DrawingPrimitives/Line.ts
+++ src/ts/DrawingPrimitives/Line.ts
@@ -1,13 +1,5 @@
 import { Point } from "../utils/Point";
-import {Color} from "../utils/Color";
-
-/**
- * Type of the brush
- *
- *  @author Josselin GIET
- *
- */
-type Brush = (center: Point, image: ImageData) => void
+import { Color } from "../utils/Color";
 
 
 
@@ -17,229 +9,131 @@ type Brush = (center: Point, image: ImageData) => void
 export class Line {
 
 
-
     /**
-     * A Drawing primitive that plots in black a pixel on the canas
+     * Draw an thick aliased segment defined by the points from and to.
      *
-     * @author Josselin GIET
-     *
-     * @param  {Point}     pixel the pixel to plot
-     * @param  {ImageData} image the ImageData to modify
+     * @param {ImageData} image     The image where the segment will be drawn.
+     * @param {Point} from          A point defining the segment.
+     * @param {Point} to            The other point defining the segment.
+     * @param {number} thickness    The segment thickness.
+     * @param {Color} color         The color of the segment.
      */
-    /*static paintItBlack(pixel: Point, image: ImageData, alpha: number):void {
-         if(pixel.x < 0 || pixel.x > image.width-1 || pixel.y < 0 || pixel.y > image.height-1) {
-             return;
-         }
-         // The color is currently random
-         let color_r = 0;
-         let color_g = 0;
-         let color_b = 0;
-
-         let coordonee1D = (pixel.y*image.width+pixel.x)*4;
-         image.data[coordonee1D] = color_r;
-         image.data[coordonee1D + 1] = color_g;
-         image.data[coordonee1D + 2] = color_b;
-         image.data[coordonee1D + 3] = Math.floor(alpha);
-     }*/
-
-
-    /**
-     * This function draw a line between the pixel from and to.
-     * It implements the algorithm of Bresenham
-     * (cf. [[https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm]])
-     * @param  {ImageData} image the image where the line is drawn
-     * @param  {Point}     from  the starting point
-     * @param  {Point}     to    the ending point
-     * @param  {toDraw}    brush the function to apply
-     *
-     * @author Josselin GIET
-     *
-     */
-    /*static draw(image: ImageData, from: Point, to: Point, brush: Brush) {
-        let currentPixel: Point = new Point(from.x, from.y);
-        brush(currentPixel, image);
-        let dx = to.x - from.x;
-        let dy = to.y - from.y;
-        let xInc = 0;
-        let yInc = 0;
-
-        // First we compute in which "direction" x and y increase
-        // between from and to
-        if (dx > 0){
-            xInc = 1;
-        }
-        else{
-            xInc = -1;
-        }
-        if (dy > 0){
-            yInc = 1;
-        }
-        else{
-            yInc = -1;
-        }
-        dx = Math.abs(dx);
-        dy = Math.abs(dy);
-
-        if (Math.abs(dx) >= Math.abs(dy)) {
-            let cumul = dx/2;
-            for(let x = from.x; x != to.x; x += xInc ){
-                currentPixel.x += xInc;
-                cumul += dy;
-                if ( cumul >= dx ){
-                    cumul -= dx;
-                    currentPixel.y += yInc;
-                }
-                brush(currentPixel, image);
-            }
-        }
-        else{
-            let cumul = dy/2;
-            for(let y = from.y; y != to.y; y += yInc){
-                currentPixel.y += yInc;
-                cumul += dx;
-                if (cumul >= dy){
-                    cumul -= dy;
-                    currentPixel.x += xInc;
-                }
-                brush(currentPixel, image);
-            }
-
-        }
-
-     //  Line.bresenham(image,from,to,10)
-    }*/
-
-    /*static bresenham(image: ImageData,from: Point, to: Point, thickness: number){
-        function setPixelColor(x,y,transparency){
-            let currentPixel: Point = new Point(x, y);
-            Line.paintItBlack(currentPixel,image,Math.floor(transparency));
-        }
-
-        // brush(currentPixel, image);
-        // First we compute in which "direction" x and y increase
-        // between from and to
-        let x0 = from.x;
-        let x1 = to.x;
-        let y0 = from.y;
-        let y1 = to.y;
-        let dx = to.x - from.x;
-        let dy = to.y - from.y;
-        let sx = dx > 0 ? 1 : -1;
-        let sy = dy > 0 ? 1 : -1;
-
-        dx = Math.abs(dx);
-        dy = Math.abs(dy);
-        let err = dx -dy;
-        let ed = dx + dy == 0? 0 : Math.sqrt(dx*dx+dy*dy);
-        let e2 = 0;
-        let x2 = 0;
-        let y2 = 0;
-
-
-        let cont: boolean = true;
-        let wd = Math.floor((thickness+1)/2);
-        // Since there is no "break" instruction in Typescript
-        while (cont ){
-            setPixelColor(x0, y0, Math.max(0,255*(Math.abs(err-dx+dy)/ed - wd + 1)));
-            e2 = err;
-            x2 = x0;
-            if(2*e2 >= -dx) {
-                e2 += dy;
-                y2 = y0;
-                while(e2 < ed*wd &&  dx > dy) {
-                    y2 += sy;
-                    setPixelColor(x0, y2, Math.max(0, 255*(Math.abs(e2)/ed - wd + 1)));
-                    e2 += dx;
-                }
-                if (x0 == x1){ cont = false}
-                e2 = err;
-                err -= dy;
-                x0 += sx;
-            }
-            if (2*e2 <= dy) {
-                e2 = dx-e2;
-                while (e2 < ed*wd && dx < dy ){
-                    x2 += sx;
-                    setPixelColor(x2, y0, Math.max(0,255*(Math.abs(e2)/ed-wd+1)));
-                    e2 += dy;
-                }
-                if (y0 == y1){ cont = false}
-                err += dx;
-                y0 += sy;
-            }
-        }
-    }*/
-
-
     static draw(image: ImageData, from: Point, to: Point, thickness: number, color: Color) {
-        if(from === to) {
+
+        // We don't draw a line if it is only a point
+        if(from.x === to.x && from.y === to.y) {
             return;
         }
 
-        let imageWidth = image.width;
-        let imageHeight = image.height;
-
+        // The corners of the bounding rectangle
         let minX = Math.min(from.x, to.x);
         let maxX = Math.max(from.x, to.x);
         let minY = Math.min(from.y, to.y);
         let maxY = Math.max(from.y, to.y);
 
-        let drawingMinX = Math.ceil(Math.max(0, minX - thickness));
-        let drawingMaxX = Math.floor(Math.min(imageWidth - 1, maxX + thickness));
-        let drawingMinY = Math.ceil(Math.max(0, minY - thickness));
-        let drawingMaxY = Math.floor(Math.min(imageHeight - 1, maxY + thickness));
+        // The corners of the intersection between the bounding rectangle and the drawing
+        let drawingMinX = Math.ceil(Math.max(0, Math.min(image.width-1, minX - thickness)));
+        let drawingMaxX = Math.floor(Math.max(0, Math.min(image.width-1, maxX + thickness)));
+        let drawingMinY = Math.ceil(Math.max(0, Math.min(image.height-1, minY - thickness)));
+        let drawingMaxY = Math.floor(Math.max(0, Math.min(image.height-1, maxY + thickness)));
 
+        // We compute the value of every pixel in the intersection of the bounding rect and the image
         for(let x = drawingMinX; x <= drawingMaxX; x++) {
-            for(let y = Math.floor(drawingMinY); y <= Math.ceil(drawingMaxY); y++) {
-                let distanceToLine = Math.sqrt(Line.distanceToLineSquared(from, to, new Point(x,y)));
-                let offset = (y * imageWidth + x) * 4;
+            for(let y = drawingMinY; y <= drawingMaxY; y++) {
 
-                if( distanceToLine < thickness - Math.sqrt(2)) {
-                    let red = image.data[offset];
-                    let blue = image.data[offset + 1];
-                    let green = image.data[offset + 2];
-                    let alpha = image.data[offset + 3];
-                    let outColor = Color.blend(color, new Color(red, blue, green, alpha));
-                    image.data[offset] = outColor.red;
-                    image.data[offset + 1] = outColor.blue;
-                    image.data[offset + 2] = outColor.green;
-                    image.data[offset + 3] = outColor.alpha;
-                } else if(distanceToLine < thickness + Math.sqrt(2)) {
-                    let nbPointsInEllipse = 0;
-                    for(let dx = -4; dx<=4; dx++) {
-                        for(let dy = -4; dy<=4; dy++) {
-                            let distanceToLineSquared = Line.distanceToLineSquared(from, to, new Point(x + dx/8, y + dy/8));
-                            if(distanceToLineSquared < thickness**2) {
-                                nbPointsInEllipse += 1;
-                            }
-                        }
-                    }
-                    let newAlpha = (nbPointsInEllipse / 81) * color.alpha;
-                    let red = image.data[offset];
-                    let blue = image.data[offset + 1];
-                    let green = image.data[offset + 2];
-                    let alpha = image.data[offset + 3];
-                    let outColor = Color.blend(new Color(color.red, color.blue, color.green, newAlpha),
-                                               new Color(red, blue, green, alpha));
-                    image.data[offset] = outColor.red;
-                    image.data[offset + 1] = outColor.blue;
-                    image.data[offset + 2] = outColor.green;
-                    image.data[offset + 3] = outColor.alpha;
-                }
+                // Get the color of this pixel, and draw it on the image
+                let pixelColor = Line.getColorForPixel(x, y, from, to, thickness, color, image);
+
+                let offset = (y * image.width + x) * 4;
+                image.data[offset]     = pixelColor.red;
+                image.data[offset + 1] = pixelColor.green;
+                image.data[offset + 2] = pixelColor.blue;
+                image.data[offset + 3] = pixelColor.alpha;
             }
         }
     }
 
-    static distanceToLineSquared(from: Point, to: Point, point: Point): number {
+
+    /**
+     * Get the color of a pixel for a segment drawing
+     *
+     * @param {number} x            The x axis of the pixel.
+     * @param {number} y            The y axis of the pixel.
+     * @param {Point} from          One segment bound.
+     * @param {Point} to            The other segment bound.
+     * @param {number} thickness    The thickness of the segment.
+     * @param {Color} color         The color of the segment.
+     * @param {ImageData} image     The image where the segment will be drawn.
+     *
+     * @returns {Color}             The color for that pixel of the line draw.
+     *
+     * @author Mathieu Fehr
+     */
+    private static getColorForPixel(x: number, y: number, from: Point, to: Point, thickness: number, color: Color, image: ImageData): Color {
+
+        // Get the distance to the segment
+        let distanceToLine = Math.sqrt(Line.distanceToSegmentSquared(from, to, new Point(x,y)));
+
+        // Get the color of the current pixel in the image
+        let offset = (y * image.width + x) * 4;
+        let destRed = image.data[offset];
+        let destGreen = image.data[offset + 1];
+        let destBlue = image.data[offset + 2];
+        let destAlpha = image.data[offset + 3];
+        let destColor = new Color(destRed, destGreen, destBlue, destAlpha);
+
+        // If we are near enough of the segment, we can draw the color with full alpha
+        if(distanceToLine < thickness - Math.sqrt(2)) {
+            return Color.blend(color, destColor);
+        }
+
+        // If we are near the border of the segment, we will use multi sampling on the pixel to compute its alpha
+        if(distanceToLine < thickness + Math.sqrt(2)) {
+
+            // We multi sample the pixel in 81 small pixels
+            let nbPointsInLine = 0;
+            for(let dx = -4; dx <= 4; dx++) {
+                for(let dy = -4; dy <= 4; dy++) {
+                    let distanceToLineSquared = Line.distanceToSegmentSquared(from, to, new Point(x + dx/8, y + dy/8));
+                    if(distanceToLineSquared < thickness ** 2) {
+                        nbPointsInLine += 1;
+                    }
+                }
+            }
+
+            // We compute the value of the new alpha we will apply on that pixel
+            let newAlpha = (nbPointsInLine / 81) * color.alpha;
+            return Color.blend(new Color(color.red, color.blue, color.green, newAlpha), destColor);
+        }
+
+        // If we are too far from the segment, we will not color it.
+        return destColor;
+    }
+
+
+    /**
+     * Get the distance to the segment defined by from and to
+     *
+     * @param {Point} from      One bound of the segment.
+     * @param {Point} to        The other bound of the segment.
+     * @param {Point} point     The point for which we want the distance.
+     * @returns {number}        The distance to the segment
+     *
+     * @author Mathieu Fehr
+     */
+    private static distanceToSegmentSquared(from: Point, to: Point, point: Point): number {
         let lengthSquared = from.distanceTo2(to);
         if(lengthSquared === 0) {
             return from.distanceTo2(point);
         }
 
+        // We compute the nearest point in the segment with a projection
         let t = ((point.x - from.x) * (to.x - from.x) + (point.y - from.y) * (to.y - from.y)) / lengthSquared;
         t = Math.max(0, Math.min(1, t));
         let projectionPoint: Point = new Point(from.x + t * (to.x - from.x),
                                                from.y + t * (to.y - from.y));
+
+        // And we return the distance between the point and its projection
         return point.distanceTo2(projectionPoint);
     }
 }
