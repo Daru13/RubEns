70bcda6a14b94f2c9f2ce1de9b1002da39219443
Daru13 <daru14@gmail.com>
2018-01-10 03:04:31 +0100

    Removed useless commented code and TODO notes.

--- src/ts/UI/RootLayout.ts
+++ src/ts/UI/RootLayout.ts
@@ -39,25 +39,5 @@ export class RootLayout extends HTMLRenderer {
         this.sidebar        = new Sidebar(this.rootNode, app);
         this.statusBar      = new StatusBar(this.rootNode, app);
         this.toolMenu       = new ToolMenu(this.rootNode, app);
-
-        // TODO: debug code, to be removed!
-        /*
-        let popupParams = new ParametersFieldPopupParameters();
-        let paramsToDisplay = Object.keys(document.parameters.sharedToolParameters)
-                                    .map((key) => document.parameters.sharedToolParameters[key])
-        let popup = new ParametersFieldPopup(this.rootNode, document, popupParams,
-                                             paramsToDisplay,
-                                             "Look at this parameter popup!",
-                                             "Try to edit those parameters...");
-        popup.onParameterChangesApplied = () => {
-            console.log("parameter changes applied");
-
-            // Notify the UI global parameters have been updated
-            let evt = new CustomEvent("rubens_globalparameterschanged", {bubbles: true});
-            this.rootNode[0].dispatchEvent(evt);
-        }
-
-        popup.show();
-        */
     }
 }
